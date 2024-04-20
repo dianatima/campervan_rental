0524a4757572f084cd95642e37f6107804324a1b
Yulia <nayanzina@gmail.com>
2024-04-20 18:09:10 +0300

    deploy

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,17 +1,13 @@
 import { Navigation } from "components/Navigation/Navigation";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import { AppBarWrap } from "./AppBar.styled";
 
 export const AppBarTop = () => {
 
   return (
     <>
-      {/* <Box sx={{ flexGrow: 1 }}> */}
         <AppBarWrap>
           <Navigation />
         </AppBarWrap>
-      {/* </Box> */}
     </>
   );
 };
